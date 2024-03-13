3acf2267145d5ec2f2d839b5e5cdb3a8ce91c208
lmalunin <lmalunin@gmail.com>
2024-03-13 18:50:56 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AuthContext, AuthProvider, IAuthContext, TAuthConfig, TRefreshTokenExpiredEvent } from "react-oauth2-code-pkce"
+
+const authConfig: TAuthConfig = {
+  clientId: 'algopack',
+  authorizationEndpoint: 'https://sso2.beta.moex.com/auth/realms/SSO/protocol/openid-connect/auth',
+  tokenEndpoint: 'https://sso2.beta.moex.com/auth/realms/SSO/protocol/openid-connect/token',
+  redirectUri: 'http://localhost:3000/',
+  scope: 'openid',
+  onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
+  autoLogin: false,
+}
+
+const UserInfo = (): JSX.Element => {
+
+  const {token, tokenData, login, logOut} = useContext<IAuthContext>(AuthContext)
+
+  return <>
+    <h4>Access Token</h4>
+    <pre>{token}</pre>
+    <h4>User Information from JWT</h4>
+    <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+
+    <button onClick={()=>login()}>Login</button>
+    <button onClick={()=>logOut()}>Logout</button>
+  </>
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider authConfig={authConfig}>
+        <UserInfo/>
+      </AuthProvider>
     </div>
   );
 }
